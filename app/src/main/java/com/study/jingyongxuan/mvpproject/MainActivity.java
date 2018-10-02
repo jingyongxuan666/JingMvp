be5c2b201c4a911391d5659c17a779636520eee5
jingyongxuan <jyx5323@163.com>
2018-10-03 00:46:57 +0800

    完善了BaseActivity的toolbar功能，明天继续完善

--- app/src/main/java/com/study/jingyongxuan/mvpproject/MainActivity.java
+++ app/src/main/java/com/study/jingyongxuan/mvpproject/MainActivity.java
@@ -2,12 +2,36 @@ package com.study.jingyongxuan.mvpproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.study.jingyongxuan.mvpproject.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+    private TextView textView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int getLayoutId(Bundle savedInstanceState) {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        textView = findViewById(R.id.textView);
+        setOnClick(textView);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        super.onClick(v);
+        switch (v.getId()) {
+            case R.id.textView:
+                break;
+            default:
+                break;
+        }
     }
 }
--- app/src/main/java/com/study/jingyongxuan/mvpproject/base/BaseActivity.java
+++ app/src/main/java/com/study/jingyongxuan/mvpproject/base/BaseActivity.java
@@ -1,19 +1,21 @@
 package com.study.jingyongxuan.mvpproject.base;
 
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -25,14 +27,14 @@ import java.util.List;
 
 import me.yokeyword.fragmentation.SupportActivity;
 
-public abstract class BaseActivity extends SupportActivity implements BaseView {
+public abstract class BaseActivity extends SupportActivity implements BaseView, View.OnClickListener {
 
     private AlertDialog loadingDialog;
     public static List<BaseActivity> activities = new ArrayList<>();
     private Toolbar toolbar;
     private TextView toolbarTitle;
-    private TextView toolbarRight;
-    private TextView tvBack;
+    private TextView txtBack;
+    private TextView txtMore;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,18 +44,43 @@ public abstract class BaseActivity extends SupportActivity implements BaseView {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         int layoutId = getLayoutId(savedInstanceState);
         //将布局文件加载到toolbar中去
-        View inflate = getLayoutInflater().inflate(R.layout.activity_base,toolbar,false);
+        View inflate = getLayoutInflater().inflate(R.layout.activity_base, toolbar, false);
         ConstraintLayout rootLayout = inflate.findViewById(R.id.ct_layout_base);
         //设置布局
-        if (layoutId == 0){
+        if (layoutId == 0) {
             //没有布局返回为0
             setContentView(rootLayout);
-        }else {
+        } else {
             //有就添加toolbar
-            View rootView = getLayoutInflater().inflate(layoutId,rootLayout,true);
+            View rootView = getLayoutInflater().inflate(layoutId, rootLayout, true);
             setContentView(rootView);
         }
         stateBar();
+        initView();
+        initToolbar(toolbar);
+        initData();
+        setOnClick(R.id.txt_back_base);
+    }
+
+    /**
+     * 如果需求和默认toolbar不同，可重写此方法
+     *
+     * @param toolbar
+     */
+    protected void initToolbar(Toolbar toolbar) {
+
+    }
+
+    protected void initData() {
+
+    }
+
+    /**
+     * 初始化控件
+     */
+    protected void initView() {
+        toolbar = findViewById(R.id.toolbar_base);
+        toolbarTitle = findViewById(R.id.txt_title_base);
     }
 
     /**
@@ -73,8 +100,75 @@ public abstract class BaseActivity extends SupportActivity implements BaseView {
         actionBar.hide();
     }
 
+    public BaseActivity setOnClick(View... views) {
+        for (View view : views) {
+            view.setOnClickListener(this);
+        }
+        return this;
+    }
+
+    public BaseActivity setOnClick(@IdRes int... ids) {
+        View view;
+        for (int id : ids) {
+            view = findViewById(id);
+            if (view != null) {
+                view.setOnClickListener(this);
+            }
+        }
+        return this;
+    }
+
+    /**
+     * 设置title
+     *
+     * @param title
+     * @return
+     */
+    public BaseActivity setTitleTxt(CharSequence title) {
+        toolbarTitle.setText(title);
+        return this;
+    }
+
+    protected BaseActivity initToolbar(boolean isShowToolbar, boolean isShowBack, boolean isShowMore) {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            if (isShowToolbar) {
+                actionBar.show();
+                txtBack = findViewById(R.id.txt_back_base);
+                txtMore = findViewById(R.id.txt_more_base);
+                if (txtBack != null && txtMore != null) {
+                    txtBack.setVisibility(isShowBack?View.VISIBLE:View.GONE);
+                    txtMore.setVisibility(isShowMore?View.VISIBLE:View.GONE);
+                }
+            }
+        }
+        return this;
+    }
+
+    /**
+     * 设置toolbar背景色
+     * @param color
+     * @return
+     */
+    public BaseActivity setToolbarBgColor(int color){
+        toolbar.setBackgroundColor(getResources().getColor(color));
+        return this;
+    }
+
+    /**
+     * 设置toolbar背景
+     * @param drawableId
+     * @return
+     */
+    public BaseActivity setToolbarBg(@DrawableRes int drawableId){
+        toolbar.setBackgroundResource(drawableId);
+        return this;
+    }
+
     /**
      * 获取当前布局对象
+     *
      * @param savedInstanceState 这个是当前activity保存的数据，最常见的就是横竖屏切换时，数据丢失问题
      * @return 当前布局的int值
      */
@@ -100,7 +194,7 @@ public abstract class BaseActivity extends SupportActivity implements BaseView {
                 window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
             }
         }
-        if (!loadingDialog.isShowing()){
+        if (!loadingDialog.isShowing()) {
             loadingDialog.show();
         }
     }
@@ -108,7 +202,7 @@ public abstract class BaseActivity extends SupportActivity implements BaseView {
     @Override
     public void hideLoading() {
         if (loadingDialog != null) {
-            if (loadingDialog.isShowing()){
+            if (loadingDialog.isShowing()) {
                 loadingDialog.dismiss();
             }
             loadingDialog = null;
@@ -128,5 +222,16 @@ public abstract class BaseActivity extends SupportActivity implements BaseView {
     @Override
     public void onBackPressedSupport() {
         super.onBackPressedSupport();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.txt_back_base:
+                onBackPressedSupport();
+                break;
+            default:
+                break;
+        }
     }
 }
